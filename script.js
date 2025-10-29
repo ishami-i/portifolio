35d9a2776123f2c27a87c2f666e3b1c922ea75e6
ishami-i <i.irene@alustudent.com>
2025-10-29 14:46:00 +0200

    javascript involved

---
+++ script.js
@@ -0,0 +1,8 @@
+// Burger menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+});
